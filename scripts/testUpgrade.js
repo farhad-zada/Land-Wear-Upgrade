4be803b2356d7d552825ad5e3b63aced3527278e
Farhad <farhad.szd@gmail.com>
2023-11-11 13:24:03 +0400

    11.11.23->up.1.0.3

---
+++ scripts/testUpgrade.js
@@ -0,0 +1,17 @@
+const { ethers, upgrades } = require("hardhat");
+const logg = require("./logg");
+require("dotenv").config();
+
+async function main() {
+  const Wear = await ethers.getContractFactory("MetawearBurnable");
+
+  console.log("Waiting the transaction... ✨");
+  const wear = await upgrades.upgradeProxy(process.env.METAWEAR_PROXY, Wear);
+  logg(wear);
+  return "success ✨";
+}
+
+main().catch((err) => {
+  console.log(err);
+  process.exitCode = 1;
+});
